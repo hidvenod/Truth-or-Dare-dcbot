b6dc54e7a004196a31b861d88357b4febebf7f65
hidvenod <87077966+hidvenod@users.noreply.github.com>
2023-07-01 22:17:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {token} = require("./config.json")
 const {Client , IntentsBitField } = require("discord.js");
+const gamefuncs = require("./commands")
 const client = new Client({
     intents:[
         IntentsBitField.Flags.Guilds,
@@ -24,45 +25,7 @@ client.once("ready", () => {
 client.on("interactionCreate", (interation) => {
     if(!interation.isChatInputCommand()) return;
     if(interation.commandName == "game"){
-        try {
-            gamerole = interation.options.get("role").role
-            probability = interation.options.get("probability").value
-            players = interation.guild.members.cache.filter(member => 
-                member.roles.cache.some( r => 
-                    r == gamerole))
-            //players is a collection that members has the gamerole
-            console.log(players.size)
-            console.log(players)
-            if((players.size<3)||(probability>100)||(probability<0)){
-                if (players.size<3){
-                    if((probability>100)||(probability<0)){
-                        rplmsg("遊玩人數須為三人以上，機率數值設定超出範圍")
-                        return
-                    }
-                    rplmsg("遊玩人數須為三人以上")
-                    return
-                }
-                rplmsg("機率數值設定超出範圍")
-                return;
-            }
-            randnum  = getRandomInt(0,players.size)
-            asknum = getRandomInt(0,players.size)
-            while(asknum == randnum){
-                asknum = getRandomInt(0,players.size)
-            }
-            playarr = Array.from(players)
-            if (getRandomInt(1, 101)<= probability){
-                mode = "大冒險"
-            }
-            else{
-                mode = "真心話"
-            }
-            ask = playarr[asknum]
-            rand = playarr[randnum]
-            rplmsg(`大冒險機率${probability}%，${ask[0]}出題，${rand[0]}${mode}`)
-            }catch(error) {
-            console.log(error);
-        }
+        gamefuncs.TruthorDare(interation)
     }
 })
 client.login(token)
